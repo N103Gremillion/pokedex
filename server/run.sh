806dbf2c0a9815010c250f4661a9070da89e01e8
N103Gremillion <nathan103grem@gmail.com>
2025-09-20 14:10:55 -0500

    added gunicorn for deployment

--- server/run.sh
+++ server/run.sh
@@ -1,5 +1,9 @@
-# ./kill.sh # kill previous running process
+
+
+PORT=${PORT:-5000}
+
+./kill.sh # kill previous running process
 
 echo "Starting backend..."
 source ./.venv/bin/activate
-python ./entry.py 
+exec gunicorn -w 4 -b 0.0.0.0:$PORT entry:app
